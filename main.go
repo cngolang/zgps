123144aa80cb7afb163a98bfac45d7b8c3008af9
inmyfree <1078092+inmyfree@users.noreply.github.com>
2018-12-08 17:20:37 +0800

    添加正式版本，和压缩版本

--- main.go
+++ main.go
@@ -18,7 +18,7 @@ func main() {
 	start := time.Now()
 	flag.IntVar(&MaxQueue, "q", -1, "最大下载个数")
 	flag.IntVar(&Threads_Maximum, "t", 10, "下载线程最大数")
-	flag.StringVar(&Down_Proxy, "dp", "", "下载文件代理方式，注意开头五socks5,只支持tcp socks5代理,如： 127.0.0.1:8888")
+	flag.StringVar(&Down_Proxy, "dp", "", "下载文件代理方式，注意开头无socks5,只支持tcp socks5代理,如： 127.0.0.1:8888")
 	flag.StringVar(&Site_Proxy, "sp", "", "获取下载地址代理方式，如： socks5://127.0.0.1:8889,http://127.0.0.1:8888")
 	flag.Parse()
 
